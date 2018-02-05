6982940fed3914f6898a0563836826dd7aa29542
Andreyskn <andskipin329@gmail.com>
2018-02-05 14:47:55 +0500

    second commit

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,34 +1,80 @@
 'use strict';
+import {tableData} from 'static/js/tableData';
 
-/*
-    This file can be used as entry point for webpack!
- */
-
-display_options.addEventListener('click', function() {
-    if (!event.target.classList.contains('is_active')) {
-        let array = [...display_options.children];
-        array.forEach(element => {
-            element.classList.remove('is_active');
-            element.querySelector('[id*="volumeChoice"]').removeAttribute('checked');
-        });
-        event.target.classList.add('is_active');
-        event.target.querySelector('[id*="volumeChoice"]').setAttribute('checked', 'checked');
-    }
-});
-
-table_head_row.addEventListener('click', function () {
-    console.log(event.target)
-})
-
-let tabsArray = [...document.body.querySelectorAll('.tabs')];
-tabsArray.forEach(element => {
-    element.addEventListener('click', function () {
-        if (!event.target.classList.contains('is_active')) {
-            let array = [...element.children];
+var displayOptions = document.getElementById('displayOptions');
+if (displayOptions) {
+    displayOptions.addEventListener('click', function() {
+        if (event.target.classList.contains('search-panel__display-options-item') && !event.target.classList.contains('is_active')) {
+            let array = [...displayOptions.children];
             array.forEach(element => {
-                element.querySelector('.tabs__link').classList.remove('is_active');
+                element.classList.remove('is_active');
             });
             event.target.classList.add('is_active');
         }
+    });
+}
+
+var mainTabs = document.getElementById('mainTabs');
+if (mainTabs) {
+    mainTabs.addEventListener('click', function() {
+        if (event.target.classList.contains('tabs__link') && !event.target.classList.contains('is_active')) {
+            handleTabsActive(mainTabs);
+        }
     })
-})
+}
+
+var extraTabs = document.getElementById('extraTabs');
+if (extraTabs) {
+    extraTabs.addEventListener('click', function () {
+        if (event.target.classList.contains('tabs__link') && !event.target.classList.contains('is_active')) {
+            handleTabsActive(extraTabs);
+        }
+    })
+}
+
+function handleTabsActive(tabsId) {
+    let array = [...tabsId.children];
+    array.forEach(el => {
+        el.querySelector('.tabs__link').classList.remove('is_active');
+    });
+    event.target.classList.add('is_active');
+}
+
+var tableHeadRow = document.getElementById('tableHeadRow');
+if (tableHeadRow) {
+    tableHeadRow.addEventListener('click', function () {
+        if (event.target.classList.contains('table__head-cell')) {
+            sortTable(event.target);
+        } else if (event.target.parentNode.classList.contains('table__head-cell')) {
+            sortTable(event.target.parentNode);
+        }
+    })
+}
+
+function sortTable(node) {
+    if (node.classList.contains('sort_up')) {
+        node.classList.remove('sort_up');
+        node.classList.add('sort_down');
+    } else if (node.classList.contains('sort_down')) {
+        node.classList.remove('sort_down');
+    } else {
+        let array = [...node.parentNode.children];
+        array.forEach(el => {
+            el.classList.remove('sort_up', 'sort_down');
+        });
+        node.classList.add('sort_up');
+        var columnIndex = array.indexOf(node);
+        var columnData = [];
+
+        tableData.forEach((row,index) => {
+            columnData.push([row[columnIndex],index]);
+        });
+        columnData.sort(function(a, b) {
+            if (a[0] < b[0]) return -1;
+            if (a[0] > b[0]) return 1;
+            return 0;
+        });
+
+        console.log(columnData);
+    }
+}
---
+++ markup/static/js/tableMaker.js
@@ -0,0 +1,76 @@
+// var content = new Object();
+var myData = new Object();
+myData.tableRows = fillTable();
+
+function fillTable() {
+    var rows = [];
+    for (i = 0; i < 30; i++) {
+        rows.push([
+            getRandomCurrency(),
+            getRandomCell2(),
+            getRandomCell3(),
+            getRandomCell4(),
+            getRandomCell5(),
+            getRandomCell67(),
+            getRandomCell67(),
+            getRandomCell89(),
+            getRandomCell89()
+        ]);
+    }
+    return rows;
+}
+
+function getRandomCurrency() {
+    var moneyArray = ['GBP', 'JPY', 'USD', 'EUR'];
+    var randomIndex = Math.floor(Math.random() * moneyArray.length);
+    var firstCurrency = moneyArray[randomIndex];
+    moneyArray.splice(randomIndex, 1);
+    var secondCurrency = moneyArray[Math.floor(Math.random() * moneyArray.length)];
+    return firstCurrency + '/' + secondCurrency
+}
+
+function getRandomCell2() {
+    var first = Math.random().toFixed(1);
+    var second = (Math.random() + 1).toFixed(1);
+    return first + ' (' + second + ')'
+}
+
+function getRandomCell3() {
+    var x = Math.floor(Math.random() * 900) + 1;
+    var x = Math.round(x / 10) * 10;
+    return x + ' USD'
+}
+
+function getRandomCell4() {
+    var x = Math.floor(Math.random() * 300) + 1
+    var x = Math.round(x / 10) * 10;
+    return x + ' 000 USD'
+}
+
+function getRandomCell5() {
+    return (Math.floor(Math.random() * 9) + 1)
+}
+
+function getRandomCell67() {
+    var min = -0.99;
+    var max = 0.99;
+    var rand = (min + Math.random() * (max - min)).toFixed(2).toString();
+    if (rand.charAt(rand.length - 1) === '0') {
+        rand = rand.substring(0, rand.length - 1);
+    }
+    if (rand == '-0.0' || rand == '0.0') {
+        rand = 0;
+    }
+    return rand
+}
+
+function getRandomCell89() {
+    var x = Math.floor(Math.random() * 900) + 100;
+    var x = Math.round(x / 10) * 10;
+    var y = Math.random().toFixed(2).toString();
+    var y = y.substring(2);
+    var result = x + ',' + y;
+    return result + ' USD'
+}
+
+console.log(myData.tableRows);
